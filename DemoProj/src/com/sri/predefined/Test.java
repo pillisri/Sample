fdf589a3acbe9e5f40544d8e4b7e968a2525534b
Srinivas <abc123@gmail.com>
2024-01-14 17:19:37 +0530

    Merge branch 'master' into test

--- DemoProj/src/com/sri/predefined/Test.java
+++ DemoProj/src/com/sri/predefined/Test.java
@@ -4,5 +4,6 @@ public class Test {
 public static void main(String[] args) {
 	System.out.println("this is demo purpose branch");
 	System.out.println("you must check in github");
+	System.out.println("hello worldl");
 }
 }
